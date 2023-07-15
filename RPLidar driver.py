9ff8550f87c5ed29c33feb64ea94c1208b2d846e
C <omar.ezzat251@email.com>
2023-07-15 15:46:03 +0300

    updated driver

--- RPLidar driver.py
+++ RPLidar driver.py
@@ -1,19 +1,22 @@
 import serial
 import math
-import matplotlib.pyplot as plt
 import time
 import pygame
-
+from scipy.spatial import KDTree
+import numpy as np
+from scipy.spatial import KDTree
+# Define colors
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 # Initialize Pygame
 pygame.init()
 
 # Set up the canvas dimensions
 width, height = 800, 600
 canvas = pygame.display.set_mode((width, height))
-
 # Set the color for the points
 point_color = (255, 0, 0)  # Red
-
 # Close any existing serial port connection on COM13 if there is one
 port = 'COM13'  # Replace with the appropriate port name
 baud_rate = 115200  # Set the baud rate to match your device
@@ -29,15 +32,35 @@ limitrange = 40
 s = serial.Serial(port, baud_rate)
 refresh_interval = 2  # Set the refresh interval in seconds
 prev_refresh_time = 0
-i = 0
+count = 0
+i = 3
 points = []
 res = 1
 image_filename = "frame.png"
 running = True
 obstical = []
 mindistance = 40
+
+# Set a distance threshold for connecting points
+threshold = 5
+i = 0  # Connection index
+linked_points = set()  # Set to store connected points
 try:
     while running:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                canvas.fill(BLACK)
+                points = []
+                obstical = []
+                linked_points = set()
+                i = 0
+                if event.key == pygame.K_UP:
+                    res = res + 1                 
+                elif event.key == pygame.K_DOWN:
+                    res = res - 1
         # Read a line of data from the serial port
         data = s.readline().decode().strip()
 
@@ -68,48 +91,61 @@ try:
             angle = round(float(parts[3]))
         except ValueError:
             continue 
-        if angle > 360 or angle in [122,123,124,125]:
+        if angle > 360 or angle in range(123, 125):
+            continue
+        if quality != 15:
             continue
 
+        count = count + reset
 
         # Convert polar coordinates to Cartesian coordinates
-        x = distance * math.cos(math.radians(angle))*res
-        y = distance * math.sin(math.radians(angle))*res
+        x = int(distance * math.cos(math.radians(angle))*res)
+        y = int(distance * math.sin(math.radians(angle))*res)
         
-        points.append((x+(width/2),y+(height/2)))
+        if count < 1 and len(points) < 500:
+            points.append((x+(width/2),y+(height/2)))
+        else:
+            print("build tree",len(points),count)
         if distance < mindistance:
             obstical.append((x+(width/2),y+(height/2)))
 
-        # # Check if it's time to refresh the plot
-        elapsed_time = time.time() - prev_refresh_time
-        if elapsed_time >= refresh_interval:
-            # Refresh the plot
-            pygame.image.save(canvas, image_filename)
-            canvas.fill((0, 0, 0))
-            points = []
-            obstical = []
-            # Store the current time as the new previous refresh time
-            prev_refresh_time = time.time()
-        # Handle events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    res = res + 1
-                elif event.key == pygame.K_DOWN:
-                    res = res - 1
 
         # Clear the canvas
-        canvas.fill((0, 0, 0))
+        if (i == len(points)):
+            print("Done",i)
+            canvas.fill(BLACK)
+            points = []
+            obstical = []
+            linked_points = set()
+            i = 0
+            count = 0          
+            
         pygame.draw.circle(canvas, (255,0, 255), (width/2,height/2), 15*res,1)
-        if len(obstical)>10:
+        if len(obstical)>50:
             pygame.draw.circle(canvas, (255,0, 0), (width/2,height/2), mindistance*res,1)
         else:
             pygame.draw.circle(canvas, (0,255, 0), (width/2,height/2), mindistance*res,1)
-        # Draw the points on the canvas
-        for point in points:
-            pygame.draw.circle(canvas, point_color, point, 1)
+
+
+        # Find nearest neighbors and connect points
+        if len(points) > 2:
+            # Build a KDTree
+            kdtree = KDTree(points)
+            # Find nearest neighbors and connect points
+            if i < len(points):
+                point = points[i]
+                neighbors = kdtree.query_ball_point(point, threshold)
+
+                for index in neighbors:
+                    if index != i:
+                        neighbor = points[index]
+                        pygame.draw.line(canvas, RED, point, neighbor, 1)
+                        linked_points.add(point)
+                        linked_points.add(neighbor)
+                        pygame.draw.circle(canvas, RED, point, 1)
+
+                i += 1
+
 
         # Update the display
         pygame.display.flip()
